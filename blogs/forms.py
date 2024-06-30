3ae4cc332be2f72bc362311b3221d2b0b03bddd8
Harshit Vijay <harshitvj07@gmail.com>
2024-06-30 18:01:11 +0530

    something

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,4 +1,3 @@
-#blogs/forms.py
 from django import forms
 from .models import BlogPost, Comment
 
@@ -6,8 +5,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control, form-check-input'}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
